8ae0dc9b5a866215daf47e375ab75f09126145cc
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-03-07 16:44:29 -0700

    made postageForm file, updated responseDisplay, updated postageCalcServer

--- nodeExpressPostageCalc/postageCalcServer.js
+++ nodeExpressPostageCalc/postageCalcServer.js
@@ -10,28 +10,167 @@ app.use(express.static("public"));
 app.set("views","viewsTemplate");
 app.set("view engine", "ejs");
 
-app.get("/", handleRootRequest);
+app.get("/", function() { 
+
+	console.log("started the landing page");
+	
+});
+
+app.get("/postage", handleRootRequest);
 /*app.get("/", function(req, res){
 	
 	console.log("received a request for /");
 	
 });*/
 
-function handleRootRequest(req, res){
-	console.log("received a request for /");
+function handlePostageRequest(req, res){
+	console.log("received a request for /postage");
 	
 	/*res.write("This is the root");
 	res.end();*/
 	
-	var name = "john";
+	var  stampedLetterVar = Number(req.query.stampedLetters);
+	var  meteredLetterVar = Number(req.query.meteredLetters);
+	var  largeEnvelopesVar = Number(req.query.largeEnvelopes);
 	
-	var emailAddress = "john@email.com";
+	var postageVar = 0;
 	
-	var params = {username: name, email: emailAddress};
+	// stamped letters
+	if(stampedLetterVar <= 1)
+	{
+		
+		postageVar = 0.55;
+		
+	}
+	if(stampedLetterVar <= 2)
+	{
+		
+		postageVar = 0.70;
+		
+	}
+	if(stampedLetterVar <= 3)
+	{
+		
+		postageVar = 0.85;
+		
+	}
+	if(stampedLetterVar <= 3.5)
+	{
+		
+		postageVar = 1.00;
+		
+	}
 	
-	console.log("received a request for /home");
+	// metered letters
+	if(meteredLetterVar <= 1)
+	{
+		
+		postageVar = 0.50;
+		
+	}
+	if(meteredLetterVar <= 2)
+	{
+		
+		postageVar = 0.65;
+		
+	}
+	if(meteredLetterVar <= 3)
+	{
+		
+		postageVar = 0.80;
+		
+	}
+	if(meteredLetterVar <= 3.5)
+	{
+		
+		postageVar = 0.95;
+		
+	}
 	
-	res.render("responseDisplay", params);
+	// large envelopes
+	if(largeEnvelopesVar <= 1)
+	{
+		
+		postageVar = 1.00;
+		
+	}
+	if(largeEnvelopesVar <= 2)
+	{
+		
+		postageVar = 1.20;
+		
+	}
+	if(largeEnvelopesVar <= 3)
+	{
+		
+		postageVar = 1.40;
+		
+	}
+	if(largeEnvelopesVar <= 4)
+	{
+		
+		postageVar = 1.60;
+		
+	}
+	if(largeEnvelopesVar <= 5)
+	{
+		
+		postageVar = 1.80;
+		
+	}
+	if(largeEnvelopesVar <= 6)
+	{
+		
+		postageVar = 2.00;
+		
+	}
+	if(largeEnvelopesVar <= 7)
+	{
+		
+		postageVar = 2.20;
+		
+	}
+	if(largeEnvelopesVar <= 8)
+	{
+		
+		postageVar = 2.40;
+		
+	}
+	if(largeEnvelopesVar <= 9)
+	{
+		
+		postageVar = 2.60;
+		
+	}
+	if(largeEnvelopesVar <= 10)
+	{
+		
+		postageVar = 2.80;
+		
+	}
+	if(largeEnvelopesVar <= 11)
+	{
+		
+		postageVar = 3.00;
+		
+	}
+	if(largeEnvelopesVar <= 12)
+	{
+		
+		postageVar = 3.20;
+		
+	}
+	if(largeEnvelopesVar <= 13)
+	{
+		
+		postageVar = 3.40;
+		
+	}
+	
+	
+	var param = {postage: postageVar};
+	res.render("responseDisplay", param);
+	
 	
 }
 
@@ -45,7 +184,7 @@ app.get("/home", function(req, res){
 	
 	console.log("received a request for /home");
 	
-	res.render("responseDisplay", params);
+	//res.render("responseDisplay", params);
 	
 });
 
