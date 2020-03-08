83c8d576c25d307a3cbc0b9d92c1f8084ce63556
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-03-07 17:01:28 -0700

    Update postageCalcServer.js

--- nodeExpressPostageCalc/postageCalcServer.js
+++ nodeExpressPostageCalc/postageCalcServer.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) { 
 
 	console.log("started the landing page");
-	res.render("postageForm");
+	//res.render("postageForm");
 	
 });
 
