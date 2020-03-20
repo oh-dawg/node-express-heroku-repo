d6ca171db26b239d9676452763172de543d45019
oh-dawg <56318871+oh-dawg@users.noreply.github.com>
2020-03-20 16:00:56 -0600

    moved functions to a new file.

--- nodeExpressPostageCalc/postageCalcServer.js
+++ nodeExpressPostageCalc/postageCalcServer.js
@@ -6,11 +6,7 @@ var express = require("express");
 var app = express();
 
 // pool
-const {Pool} = require("pg");
 
-//connection string to connect to a database
-const myConnectionString = process.env.DATABASE_URL || "postgres://kmkadjqhvmxugg:f04afeed7007ac3f3c39f7dd165c2ca20ec541b7942b032d92b7c2bd96eb1bc6@ec2-52-45-14-227.compute-1.amazonaws.com:5432/dfjeaerva4s9th?ssl=true";
-const pool = new Pool ({connectionString: myConnectionString});
 
 app.use(express.static("public"));
 
@@ -33,45 +29,6 @@ app.get("/collageproject", function(req, res) {
 	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! i need to call getCollage
 	
 });
-
-function getCollage(req, res){
-	
-	console.log("i got into the collage project function");
-	
-	var result = {id: 123, picturedetail: "vacation", picturedetail2: "pet", picturedetail3: "nature"};
-	
-	res.json(result);
-	
-	var id = req.query.id;
-	// i think if i want to emulate the video i put getCollageFromDB in here?
-		getCollageFromDb(picturedetail, function(error, result) {
-	
-		console.log("retrieving item from the DB");
-	
-		// SELECT * from collagetable;
-	
-		});
-	
-}
-
-function getCollageFromDb(picturedetail, callback){
-	console.log("getPersonFromDB");
-	
-	
-	var sql = "SELECT id, picturedetail1, picturedetail2, picturedetail3, ppicturefilename FROM collagetable WHERE id = $1::int";
-	var params = [id];
-	pool.query(sql, params, function(err, result){
-		if(err){
-			console.log("an error with the db occured");
-			
-		}
-		
-		console.log("found db result" + JSON.strinify(result.rows));
-		callback(null, result.rows);
-		
-	});
-		
-}
 
 app.get("/postage", handlePostageRequest);
 /*app.get("/", function(req, res){
